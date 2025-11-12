5d385daa1b6614a9dd3c0f0a4ef02099ea725757
Julio <julio17cs@gmail.com>
2025-11-11 22:02:56 -0300

    Adicionando os modelos de dados borrowing

--- biblioteca/borrowing/models.py
+++ biblioteca/borrowing/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from catalog.models import Book
+import datetime
 
 # Create your models here.
+class BorrowedBook(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrow_date = models.DateField(auto_now_add=True)
+    due_date = models.DateField(default=datetime.date.today() + datetime.timedelta(days=14))
+    returned = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user} - {self.book}"
