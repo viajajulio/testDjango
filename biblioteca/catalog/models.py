d0d49482ebb82e87d74277f00aada4ffac84f522
Julio <julio17cs@gmail.com>
2025-11-11 22:01:22 -0300

    Adicionando os modelos de dados

--- biblioteca/catalog/models.py
+++ biblioteca/catalog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=256)
+    author = models.CharField(max_length=256)
+    isbn = models.CharField(max_length=13, unique=True)
+    total_copies = models.PositiveIntegerField(default=1)
+    available_copies = models.PositiveIntegerField(default=total_copies)
+
+    def __str__(self):
+        return self.title
