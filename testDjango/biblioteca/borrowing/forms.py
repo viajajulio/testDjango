02fba2bf4267fa6023795bdb6409a2a5b4f2a7be
Julio <julio17cs@gmail.com>
2025-11-14 17:45:55 -0300

    atualizando o forms.py do borrowing para adicionar o registro de usuario

--- testDjango/biblioteca/borrowing/forms.py
+++ testDjango/biblioteca/borrowing/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import BorrowedBook
 from catalog.models import Book
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm
 
 User = get_user_model()
 
@@ -11,3 +12,12 @@ class BorrowBookForm(forms.ModelForm):
     class Meta:
         model = BorrowedBook
         fields = ['book']
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
+
+        
