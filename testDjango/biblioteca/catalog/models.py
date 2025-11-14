7eb6694ee7e22acef877f04b9eb1b25ec2ac2c0a
Julio <julio17cs@gmail.com>
2025-11-14 17:18:21 -0300

    FIX - Ajustando para salvar o livro corretamente

--- testDjango/biblioteca/catalog/models.py
+++ testDjango/biblioteca/catalog/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=256)
-    author = models.CharField(max_length=256)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
     isbn = models.CharField(max_length=13, unique=True)
     total_copies = models.PositiveIntegerField(default=1)
-    available_copies = models.PositiveIntegerField(default=total_copies)
+    available_copies = models.PositiveIntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        if not self.id:  # Only set on creation
+            self.available_copies = self.total_copies
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
