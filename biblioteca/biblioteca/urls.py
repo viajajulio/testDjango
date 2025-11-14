897fb4665615af3b2bb4afc20cd4c9ad533a96dd
Julio <julio17cs@gmail.com>
2025-11-13 22:31:57 -0300

    adicioando as URLs de redirecionamento

--- biblioteca/biblioteca/urls.py
+++ biblioteca/biblioteca/urls.py
@@ -14,9 +14,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include
 from django.contrib import admin
 from django.urls import path
+from catalog import views as catalog_views
+from borrowing import views as borrowing_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+
+# Catalog
+    path('', catalog_views.book_list, name='book_list'),
+    path('add-book/', catalog_views.add_book, name='add_book'),
+    
+# Borrowing
+    path('borrow/', borrowing_views.borrow_book, name='borrow_book'),
+    path('return/<int:pk>/', borrowing_views.return_book, name='return_book'),
+    path('my-books/', borrowing_views.my_books, name='my_books'),
+    path('overdue/', borrowing_views.overdue_books, name='overdue_books'),
+
+# Auth
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
